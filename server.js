45c019ce82e97958b039f7a25cbb37f33c5556d0
Naman Jain <namanb139j@gmail.com>
2021-03-16 13:02:20 +0530

    gitt

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const mongoose=require('mongoose')
 const Disease=require('./app/models/disease')
 const Post=require('./app/models/post')
 app.use(express.json())
+
+var multer = require('multer');
+ 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+var upload = multer({ storage: storage });
+
+
+
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false})
 
 const connection=mongoose.connection
