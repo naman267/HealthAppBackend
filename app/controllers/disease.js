bab29d8006f48c6b53a6ecdcbc9baf62d2b9dbbf
Naman Jain <namanb139j@gmail.com>
2021-03-19 14:41:18 +0530

    less data

--- app/controllers/disease.js
+++ app/controllers/disease.js
@@ -3,11 +3,7 @@ const Disease = require('../models/disease')
 function controller() {
   return {
     diseaseList(req, res) {
-      const dis = new Disease({
-        title: 'sar dard',
-        category: 'internal'
-      })
-      dis.save()
+      
 
       Disease.find({}).then((disease) => {
         console.log(disease)
